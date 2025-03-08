f224f7202a4d6ab41c44579b3f12d7cbf13548a8
PajiloyEvrei <redki.evrei@gmail.com>
2025-03-09 00:08:05 +0300

    sdg

---
+++ html/js/bar.js
@@ -0,0 +1,7 @@
+function mOver() {
+ //document.getElementById('progressBar').style.height = '40px';
+}
+
+function mOut() {
+ //document.getElementById('progressBar').style.height = '5px';
+}
--- html/js/planet.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('mousemove', e => {
-	document.querySelector(".layer-2").style.transform = 'rotate(' + (e.clientX / 360) + 'deg)';
-})
-
