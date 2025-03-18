637749e4a9c1a13cd197cd7cebb5dc933ebc083d
PajiloyEvrei <redki.evrei@gmail.com>
2025-03-18 16:35:25 +0300

    апв

--- html/js/app.js
+++ html/js/app.js
@@ -10,7 +10,7 @@
 let canvas = document.getElementById('mainFrame');
 const ctx = canvas.getContext("2d");
 const image = new Image();
-image.src = 'img/SPB_yellow_map_1882.png';
+image.src = 'img/by_SPB_yellow_map_1882.png';
 canvas.width = 2000;
 canvas.height = 1000;
 requestAnimationFrame(drawCanvas);
@@ -22,6 +22,7 @@ let dirty = true;
 const mouse = {x: 0, y: 0, oldX: 0, oldY: 0, dragging: false};
 
 canvas.addEventListener("mousemove", mouseEvent, {passive: true});
+canvas.addEventListener("mouseover", mouseEvent, {passive: true});
 canvas.addEventListener("mousedown", mouseEvent, {passive: true});
 canvas.addEventListener("mouseup", mouseEvent, {passive: true});
 canvas.addEventListener("mouseout", mouseEvent, {passive: true});
@@ -41,42 +42,15 @@ function update() {
     matrix[5] = pos.y;
 }
 
+
 function pan(amount) {
     if (dirty)
         update();
-    let c = document.getElementById("center").getBoundingClientRect();
-    let power = 1;
-    if(amount.x > c.left && pos.x > -200){
-    	if(amount.x/c.left > 1.5){
-    		pos.x -= amount.x/c.left * 3 * power;
-    	}
-    	else {
-    		pos.x -= amount.x/c.left * 2 * power;
-    	}
+    if(pos.x + amount.x > -200 && pos.x + amount.x < 200){
+        pos.x += amount.x;
     }
-    else if(amount.x  < c.left && pos.x < 200){
-    	if(amount.x/c.left > 1.5){
-    		pos.x += c.left/(amount.x + 50) * 3 * power;
-    	}
-    	else {
-    		pos.x += c.left/(amount.x + 50) * power;
-    	}
-    }
-    if(amount.y > c.top && pos.y > -200){
-    	if(amount.y/c.top > 1.3){
-    		pos.y -= amount.y/c.top * 3 * power;
-    	}
-    	else {
-    		pos.y -= amount.y/c.top * 2 * power;
-    	}
-    }
-    else if(amount.y  < c.top && pos.y < 200){
-    	if(amount.y/c.top > 1.2){
-    		pos.y += c.top/(amount.y + 50) * 3 * power;
-    	}
-    	else {
-    		pos.y += c.top/(amount.y + 50) * 2 * power;
-    	}
+    if(pos.y + amount.y > -200 && pos.y + amount.y < 200){
+        pos.y += amount.y;
     }
     dirty = true;
 }
@@ -101,9 +75,14 @@ function drawCanvas() {
 }
 
 function mouseEvent(e) {
+    if (e.type === "mousedown")
+        mouse.dragging = true;
+    if (e.type === "mouseup" || e.type === "mouseout")
+        mouse.dragging = false;
     mouse.oldX = mouse.x;
     mouse.oldY = mouse.y;
     mouse.x = e.offsetX;
-    mouse.y = e.offsetY;
-    pan({x: mouse.x , y: mouse.y });
+    mouse.y = e.offsetY
+    if (mouse.dragging)
+        pan({x: mouse.x - mouse.oldX, y: mouse.y - mouse.oldY});
 }
